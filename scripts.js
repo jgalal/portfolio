94267360873875c285fdba2217a8c3000007c680
Jihad Galal <j.galal@mail.utoronto.ca>
2024-05-23 13:10:00 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/* scripts.js */
+function showSection(id) {
+    document.querySelectorAll('section').forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+}
