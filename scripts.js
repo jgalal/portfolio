86a3aa27f81cb7f14ef8cd75f8323bff00127e45
Jalal G <jalalg@Jalals-MacBook-Air.local>
2024-10-19 22:24:18 -0400

    Updated HTML, CSS, and JS files with improved styling and effects

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 // Smooth scrolling effect for navigation links
 $(document).ready(function() {
   $('a.nav-link').on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      const hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800);
-    }
+      if (this.hash !== "") {
+          event.preventDefault();
+          const hash = this.hash;
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top
+          }, 800);
+      }
   });
 
   // Fade-in effect for sections
   $(window).on('scroll', function() {
-    $('.fade-in').each(function() {
-      const elementPos = $(this).offset().top;
-      const windowTop = $(window).scrollTop();
-      if (elementPos < windowTop + $(window).height() - 100) {
-        $(this).addClass('visible');
-      }
-    });
+      $('.fade-in').each(function() {
+          const elementPos = $(this).offset().top;
+          const windowTop = $(window).scrollTop();
+          if (elementPos < windowTop + $(window).height() - 100) {
+              $(this).addClass('visible');
+          }
+      });
   });
 });
